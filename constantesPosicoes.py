554eb1751517f4e29ec9a34b7f4b164b5ad73418
devTav <gustavoluizsantosfaria@gmail.com>
2025-04-28 11:39:38 -0300

    Arquivo de constantes para armazenar posicoes cirado

---
+++ constantesPosicoes.py
@@ -0,0 +1,57 @@
+PONTO_REF = (200, 100) # Ponto de referencia do script para sair de uma janela de filtro
+
+# ---- CONSTANTES DE POSIÇÃO DOS DROPDOWNS DOS FILTROS GERAIS ----
+POSICAO_POR_UF                  = (600, 250)   # macro PorUF
+POSICAO_POR_CNPJ                = (1300, 250)  # macro PorCNPJ
+
+POSICAO_DO_ANO                  = (200, 150)   # Dropdown Ano
+POSICAO_DO_MES                  = (200, 200)   # Dropdown Mês
+POSICAO_DO_CADUNICO             = (200, 250)   # Dropdown CadÚnico
+POSICAO_DA_BOLSA_FAMILIA        = (200, 300)   # Dropdown Bolsa Família
+POSICAO_DA_SITUACAO_DE_POBREZA  = (200, 350)   # Dropdown Situação de Pobreza
+POSICAO_DO_SETOR_ECONOMICO      = (200, 400)   # Dropdown Setor Econômico
+POSICAO_DA_UF                   = (200, 455)   # Dropdown UF
+POSICAO_DO_MUNICIPIO            = (200, 520)   # Dropdown Município
+POSICAO_DO_SEXO                 = (200, 560)   # Dropdown Sexo
+POSICAO_DA_RACA                 = (200, 620)   # Dropdown Raça/Cor
+POSICAO_DO_GRAU_DE_INSTRUCAO    = (200, 670)   # Dropdown Grau de Instrução
+POSICAO_DA_FAIXA_ETARIA         = (200, 720)   # Dropdown Faixa Etária
+
+# ---- CONSTANTES DE POSICAO DOS FILTROS ESPECIFICOS ----
+
+# posicoes filtro bolsa familia
+POSICAO_BF_SIM = (90, 450)
+POSICAO_BF_NAO = (90, 420)
+
+# posicoes filtro situacao de pobreza
+POSICAO_SP_SIM = (90, 500) 
+POSICAO_SP_NAO = (90, 475) 
+
+# posicoes filtro setor economico
+POSICAO_SE_AGR = (100, 525) # setor AGRONEGOCIO
+POSICAO_SE_CMR = (100, 555) # setor COMERCIO
+POSICAO_SE_CST = (100, 585) # setor CONSTRUCAO
+POSICAO_SE_IND = (100, 610) # setor INDUSTRIA
+POSICAO_SE_SER = (100, 640) # setor SERVICOS
+
+# posicoes filtro sexo
+POSICAO_SX_HOM = (150, 680) # sexo HOMEM
+POSICAO_SX_MUL = (150, 710) # sexo MULHER
+POSICAO_SX_NID = (150, 740) # sexo NAO IDENTIFICADO
+
+# posicoes filtro cor/raca
+POSICAO_CR_AMA = (150, 735) # cor AMARELA
+POSICAO_CR_BRC = (150, 765) # cor BRANCA
+POSICAO_CR_IND = (150, 795) # cor INDIGENA
+POSICAO_CR_NID = (150, 820) # cor NAO IDENTIFICADA
+POSICAO_CR_NIN = (150, 850) # cor NAO INFORMADA
+POSICAO_CR_PRD = (150, 880) # cor PARDA
+POSICAO_CR_PRT = (150, 910) # cor PRETA
+
+# --- CONSTANTES DA FUNÇÃO exportarDados ----
+PONTO_REF_TABELA    = (1300 , 400)
+POSICAO_ITEM_TABELA = (1305 , 405)
+POSICAO_ITEM_BAIXAR = (1305 , 600)
+POSICAO_ITEM_DADOS  = (1305 , 500)
+POSICAO_EXPORTAR    = (1100 , 600)
+POSICAO_LINK        = (900  , 550)
